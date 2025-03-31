22c8cabc3fe79e96b4127d1e8968b69d288693b3
isla <mislambek7@gmail.com>
2025-04-01 03:32:50 +0700

    Добавлены миграции для diary и mood_tracker

---
+++ src/migrations/versions/2025_04_01_0314-c1c9aa0bbe2a_add_diary_model_and_mood_tracker.py
@@ -0,0 +1,47 @@
+"""add diary model and mood tracker
+
+Revision ID: c1c9aa0bbe2a
+Revises: 2b7f6bfd7b94
+Create Date: 2025-04-01 03:14:35.531830
+
+"""
+import uuid
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "c1c9aa0bbe2a"
+down_revision: Union[str, None] = "2b7f6bfd7b94"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "diary",
+        sa.Column("id", sa.UUID(), nullable=False, default=uuid.uuid4),
+        sa.Column("text", sa.String(), nullable=False),
+        sa.Column("created_at", sa.DateTime(), nullable=False),
+        sa.Column("user_id", sa.UUID(), nullable=False),
+        sa.ForeignKeyConstraint(["user_id"], ["users.id"], name="fk_diary_user_id"),
+        sa.PrimaryKeyConstraint("id"),
+        sa.Comment("Таблица для хранения дневниковых записей")
+    )
+
+    op.create_table(
+        "mood_tracker",
+        sa.Column("id", sa.UUID(), nullable=False, default=uuid.uuid4),
+        sa.Column("score", sa.Integer(), nullable=False),
+        sa.Column("created_at", sa.DateTime(), nullable=False),
+        sa.Column("user_id", sa.UUID(), nullable=False),
+        sa.ForeignKeyConstraint(["user_id"], ["users.id"], name="fk_mood_tracker_user_id"),
+        sa.PrimaryKeyConstraint("id"),
+        sa.Comment("Таблица для отслеживания настроения")
+    )
+
+def downgrade() -> None:
+    op.drop_table("mood_tracker")
+    op.drop_table("diary")
